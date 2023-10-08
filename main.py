737a6dd9bb46ff6d27adb3ffe05353e68f34eebb
bsarifhsu <b_sari@mail.fhsu.edu>
2023-10-07 22:00:09 -0500

    Comments added.

--- main.py
+++ main.py
@@ -5,20 +5,30 @@
 import streamlit as st
 import yfinance as yf
 
+# Sidebar
 my_text = st.sidebar.text_input("Enter a symbol", "AAPL", max_chars=4)
+
+# Main Page
 st.header("A simple financial dashboard.")
-st.write("An honest trial to learn more about Streamlit. ")
+st.write("An honest trial to learn more about Streamlit.")
 
-
+# Getting stock data for the specific ticker
 stock = yf.Ticker(my_text)
 
+# Image that displays info about the ticker
 url = f"https://charts.finviz.com/chart.ashx?t={my_text}"
 
+# Projecting the retrieved image
 st.image(url)
+
+# Displays short information about the business
 st.write(stock.info["longBusinessSummary"])
 
+# Retrieving the last 1 month of history and display it
 hist = stock.history(period="1mo")
 st.write(hist)
+
+# Creating a chart of the history retrieved.
 st.line_chart(hist)
 
 
